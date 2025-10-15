6fa4d45cf25c66c681e6c8a652d825ab5831d513
Jarozin <mishamiler90@gmail.com>
2025-10-16 01:43:00 +0300

    fix test location

--- e2e/runner_test.go
+++ e2e/runner_test.go
@@ -2,19 +2,20 @@ package e2e
 
 import (
 	"context"
-	"github.com/ozontech/allure-go/pkg/framework/runner"
-	"github.com/ozontech/allure-go/pkg/framework/suite"
 	"net/http"
 	"path/filepath"
 	"src/cmd/muzyaka"
 	dbhelpers "src/internal/lib/testing/db"
 	"sync"
 	"testing"
+
+	"github.com/ozontech/allure-go/pkg/framework/runner"
+	"github.com/ozontech/allure-go/pkg/framework/suite"
 )
 
 func TestRunner(t *testing.T) {
 	t.Parallel()
-	dbMeta, err := dbhelpers.CreateDatabase(context.Background(), filepath.Join("..", "..", "database", "docker-entrypoint-initdb.d", "01-init.sql"))
+	dbMeta, err := dbhelpers.CreateDatabase(context.Background(), filepath.Join("..", "database", "docker-entrypoint-initdb.d", "01-init.sql"))
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a test database connection", err)
 	}
